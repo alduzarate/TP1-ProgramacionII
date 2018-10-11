57dfb3b54ec24d4ba1dab3c3d0472b067246c1be
delfi-martin <31671160+delfi-martin@users.noreply.github.com>
2018-10-11 12:11:44 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
+import random
+
 def principal():
     menu()
 
@@ -7,35 +9,70 @@ def menu():
 		key = input("""
 Menu:
 1-Generar una sopa de letras
-2-Mostrar una sopa de letras
-3-Resolver una sopa de letras
+2-Resolver una sopa de letras
 (Ctrl+Z) -> salir
 
 """)
-		if key != "1" and key != "2" and key != "3":
+		if key != "1" and key != "2":
 			print ("\nLa tecla", key , "no es válida\n")
 
 		if key == "1":
 			generar()
 		elif key == "2":
-			mostrar()
-		elif key == "3":
 			resolver()
 
 def generar():
-	print("generarfunction")
+	palabras = archivo()
+	#print (palabras)
+	
+	maxPalabra = len ( max ( palabras , key=len ) )
+	cantPalabras = len ( palabras )
+	
+	datos = generarTablero ( maxPalabra )
+	tab = datos[0]
+	dim = datos[1]
+
+	mostrar ( tab , dim )
+
+def generarTablero( maxPalabra ):
+	"""El tablero esta representado con una lista de tuplas en donde la primera posicion
+	es un numero que representa la posicion y el segundo un numero que representa el valor
+	en esa posicion"""
+	dimension = random.randint ( 1.5 * maxPalabra , 2 * maxPalabra)
+	print (dimension)
+	tablero = []
+
+	for i in range ( pow ( dimension , 2 ) ):
+		tablero += [ ( i , -1 ) ]
+
+	return ( tablero , dimension )
+
+
 def resolver():
 	print("resolverfunction")
-def archivoIn():
-	archivo  = open(bdd.txt , mode = 'w')
-def archivoOut():
-	archivo  = open(bdd.txt , mode = 'r+')
+
+
+def archivo():
+	archivo  = open("bdd.txt" , "r+" )
+	palabras = archivo.readlines()
+	return palabras
+
+
 def insertar():
 	print("insertarfunction")
 def rellenar():
 	print("rellenarfunction")
-def mostrar():
-	print("mostrarfunction")
+
+
+def mostrar( tablero , dimension ):
+
+	print (tablero)
+
+#	for i in tablero:
+#		print ( " " , tablero[i][0] , end = '')
+#		if i[0] % dimension == dimension - 1:
+#			print ()
+
 def buscar():
 	print("buscarfunction")
 
